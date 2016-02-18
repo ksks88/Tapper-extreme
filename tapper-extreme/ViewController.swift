fe01aac0f9d09f84cf357e04a7031325ccd2eff2
Salih <salih.hajlakovic@gmail.com>
2016-02-18 22:58:21 +0100

    Tapper app completed

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,15 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Variables
+    var maxTaps = 0
+    var currentTap = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var txtTaps: UITextField!
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    @IBOutlet weak var btnTap: UIButton!
+    @IBOutlet weak var lblTotalTaps: UILabel!
+    
+    @IBAction func onPlayBtnClick(sender: UIButton!)
+    {
+        if(txtTaps.text != nil && txtTaps.text != "")
+        {
+            clearScreen(true)
+            
+            maxTaps = Int(txtTaps.text!)!
+            resetScores()
+            updateLabel()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func OnTap(sender: UIButton!)
+    {
+        currentTap++
+        
+        if maxTaps > currentTap
+        {
+            updateLabel()
+        }
+        else
+        {
+            clearScreen(false)
+            resetScores()
+        }
+    }
+    
+    func resetScores()
+    {
+        txtTaps.text = ""
+        currentTap = 0
+    }
+    
+    func clearScreen(IsGoingToSecondScreen: Bool)
+    {
+        logoImg.hidden = IsGoingToSecondScreen
+        txtTaps.hidden = IsGoingToSecondScreen
+        btnPlay.hidden = IsGoingToSecondScreen
+        
+        btnTap.hidden = !IsGoingToSecondScreen
+        lblTotalTaps.hidden = !IsGoingToSecondScreen
+    }
+    
+    func updateLabel()
+    {
+        lblTotalTaps.text = "\(currentTap) Taps"
     }
 
 
